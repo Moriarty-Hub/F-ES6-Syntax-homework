2ccef83c6827edff6ccfe6db3f68e1f2bb706388
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-09-10 22:03:23 +0800

    [xdong] finish practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
+const parseData = (input) => {
+    let resultList = [];
+    input.data.forEach(person => {
+        let result = {};
+        person.forEach((value, i) => {
+            result[input.column[i].name] = value;
+        })
+        resultList.push(result);
+    })
+    return resultList;
+}
 export { parseData };
