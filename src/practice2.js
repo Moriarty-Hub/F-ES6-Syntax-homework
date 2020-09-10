6e8a29d29527ef33b268084f51b0fd720daca643
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-09-10 21:51:41 +0800

    [xdong] finish practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
+const inject = (targetArray, inputArray) => {
+    let offset = 0;
+    inputArray.forEach((object) => {
+        targetArray.splice(object.index + offset, 0, object.content);
+        offset += 1;
+    })
+    return targetArray;
+}
 export { inject };
